69d3af7ef7b8d87ea637bb9b6ff514c01063bd71
REstorninho <95037857+REstorninho@users.noreply.github.com>
2026-06-09 00:07:02 +0000

    Refactor win-update-reset.bat to use PowerShell
    Updated batch script to use PowerShell commands for stopping, removing, and starting services.

--- win-update-reset.bat
+++ win-update-reset.bat
@@ -6,14 +6,14 @@ echo Windows Update Repair Tool
 echo ====================================
 echo.
 echo [1/4] Stop services
-net stop wuauserv | net stop cryptSvc | net stop bits | net stop msiserver
+Stop-Service "wuauserv" -Force | Stop-Service "cryptSvc" -Force | Stop-Service "bits" -Force | Stop-Service "msiserver" -Force
 echo.
 echo [2/4] Delete Windows Update Repository
 echo.
-del /S /F /AH “C:\Windows\SoftwareDistribution” | del /S /F /AH “SoftwareDistribution.old”
+remove-item “C:\Windows\SoftwareDistribution” -recurse | remove-item “SoftwareDistribution.old” -recurse
 echo [3/4] Start Services
 echo.
-net start wuauserv | net start cryptSvc | net start bits | net start msiserver
+Start-Service "wuauserv" | Start-Service "cryptSvc" | Start-Service "bits" | Start-Service "msiserver"
 echo.
 echo [4/4] Checking Windows Update
 wuauclt.exe /detectnow
